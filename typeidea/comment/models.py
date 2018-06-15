c6fbb96fc64adb1fe761aab191e4e6aba865bb99
nishuideyu <1336038150@qq.com>
2018-06-15 23:47:08 +0800

    添加migrations

--- typeidea/comment/models.py
+++ typeidea/comment/models.py
@@ -3,13 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from blogs.models. import Post
+from blog.models import Post
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="文章")
     content = models.CharField(max_length=2000, verbose_name="内容")
-    nickname = models.CHarField(max_lenght=50, verbose_name="昵称")
+    nickname = models.CharField(max_length=50, verbose_name="昵称")
     website = models.URLField(verbose_name="网站")
     email = models.EmailField(verbose_name="邮箱")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
